468f104e6bd5813739dea64eddc6906e09f18923
Hyunseokjung <hyunsukgo@naver.com>
2019-11-12 16:18:19 +0900

    ecr push
    ecr push

--- scripts/002-install-deps.sh
+++ scripts/002-install-deps.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 APP_ROOT=/home/ec2-user/idexport
 cd $APP_ROOT
+$(aws ecr get-login --no-include-email --region ap-northeast-2)
+docker build -t idexport:latest .
+docker tag idexport:latest 533616270150.dkr.ecr.ap-northeast-2.amazonaws.com/idexport:latest
+docker push 533616270150.dkr.ecr.ap-northeast-2.amazonaws.com/idexport:latest
 npm install
